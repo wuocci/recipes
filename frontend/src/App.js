0a535f7fa6d937a50a1b28e9990d7861467c69dd
Hannes Wuorinen <hannes.wuorinen@tuni.fi>
2021-08-09 22:57:34 +0300

    add services file and login token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import './css/App.css';
+import React, {useState, useEffect} from 'react';
 import FrontPage from './pages/frontPage/FrontPage.js';
 import LoginPage from './pages/logInPage/LoginPage.js';
 import SignUpPage from './pages/signUpPage/SignUpPage.js';
+import recipeService from './services.js' 
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,7 +11,32 @@ import {
   Link
 } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null)
+  const [recipes, setRecipes] = useState([])
+
+
+  //get all recipes from database
+  useEffect(() => {
+    recipeService.getAll().then(recipes =>
+      setRecipes(recipes)
+    )
+  }, [])
+
+
+  //get user data when page renders
+  useEffect(() => {    
+    const loggedUserJSON = window.localStorage.getItem('loggedRecipeAppUser')    
+    if (loggedUserJSON) {      
+      const user = JSON.parse(loggedUserJSON)      
+      setUser(user)      
+      recipeService.setToken(user.token)    
+    }  
+  }, 
+  [])
+
+
+
   return (
     <Router>
       <div>
--- frontend/src/components/forms/LoginForm.js
+++ frontend/src/components/forms/LoginForm.js
@@ -1,13 +1,16 @@
 import React, { useRef, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Notification from '../../Notification'
+import Notification from '../../Notification';
+import recipeService from '../../services.js' 
 
 
 const LoginForm = () => {
-    const [username, setUsername] = useState(null)
-    const [password, setPassWord] = useState(null)
+    const [username, setUsername] = useState('')
+    const [password, setPassWord] = useState('')
+    const [user, setUser] = useState(null)
     const showError = useRef(false)
+
 
     const handleUsername = (event) => {
         setUsername(event.target.value);
@@ -18,15 +21,25 @@ const LoginForm = () => {
     };
 
      //simple login check for later authentication and stuff
-    const checkLogin = () => {
-        if(username === null || password === null){ 
-           console.log("invalid")
-        }
-        else{
-            console.log(username)
-            console.log(password)
-        }
-    }
+     const checkLogin = async (event) => {   
+        event.preventDefault()
+        try {      
+          const user = await recipeService.login({ username, password })
+          recipeService.setToken(user.token)   
+          window.localStorage.setItem(        
+            'loggedNoteappUser', JSON.stringify(user)      
+            ) 
+            setUser(user)    
+            setUsername('')      
+            setPassWord('')    
+          } 
+          catch (exception) {   
+            setTimeout(() => {          
+          }, 5000)    
+          }
+          setPassWord("")
+          setUsername("")
+      }
 
     return (
         <div className="login-form">
