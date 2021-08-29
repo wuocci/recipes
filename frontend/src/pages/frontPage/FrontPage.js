6ebda977539949de269cd685002fb0066f3e7933
Pietari pakarinen <Pietari.Pakarinen@tuni.fi>
2021-08-29 21:05:07 +0300

    resolve issues

--- frontend/src/pages/frontPage/FrontPage.js
+++ frontend/src/pages/frontPage/FrontPage.js
@@ -1,20 +1,14 @@
 import ButtonAppBar from '../../components/appBar/AppBar.js';
 import SearchBar from '../../components/SearchBar.js';
 import RecipeGrid from '../../components/RecipeGrid.js';
-<<<<<<< HEAD
-=======
 import { Divider } from '@material-ui/core';
->>>>>>> c870df0916fff3bfff5a8152c736df6c2657e320
 
 const FrontPage = () => {
     return (
         <div className="FrontPage">
             <ButtonAppBar isProfilePage={false} />
             <SearchBar />
-<<<<<<< HEAD
-=======
             <Divider/>
->>>>>>> c870df0916fff3bfff5a8152c736df6c2657e320
             <RecipeGrid/>
         </div>
     );
