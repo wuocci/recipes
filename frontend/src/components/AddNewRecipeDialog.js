9c9a8b9e78b281363bd3cf4fe5585800b82099ef
Pietari pakarinen <Pietari.Pakarinen@tuni.fi>
2021-09-24 18:41:53 +0300

    add new recipe dialog and opening it

---
+++ frontend/src/components/AddNewRecipeDialog.js
@@ -0,0 +1,78 @@
+import React, { useState} from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Close from '@material-ui/icons/Close';
+
+
+const RecipeDialog = ({openDialog, toggleModal}) => {
+  console.log(openDialog)
+  return (
+    <div>
+      <Dialog
+        open={openDialog}
+        /*
+        Would need to be modified to detect if click occured in our out of the actual dialog.
+        I don't think it's necessary to have this alternate way of closing the dialog anyway.
+        
+        onClick={() => toggleModal(false)}
+        */
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        className="recipe-dialog"
+      >
+        
+          <DialogContent class="recipeDialogContent">
+          <div class="recipeContainer">
+            <DialogTitle class="recipeTitle" id="alert-dialog-title">{"Recipe name by user user"}
+              <IconButton onClick={() => toggleModal(false)} color="primary">
+                <Close />
+              </IconButton>
+            </DialogTitle>
+
+            <DialogContentText class="recipeSlides">
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+            </DialogContentText>
+
+            <DialogContentText class="recipeSpecs">
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+            </DialogContentText>
+
+            <DialogContentText class="recipeInstructions">
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+            </DialogContentText>
+
+            <DialogContentText class="recipeIngredients">
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+            </DialogContentText>
+
+            <DialogActions class="recipeFavorite" >
+              <Button color="primary" autoFocus>
+                Add favourite
+              </Button>
+            </DialogActions>
+            </div>
+          </DialogContent>
+
+      </Dialog>
+    </div>
+  );
+}
+
+export default RecipeDialog;
--- frontend/src/components/appBar/ButtonAppBar.js
+++ frontend/src/components/appBar/ButtonAppBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +6,7 @@ import logo from '../../img/logoAppbar.svg';
 import LogInButton from './LogInButton';
 import AuthService from '../../services/authservice'
 import SignUpButton from './SignUpButton';
+import RecipeDialog from '../RecipeDialog';
 import { useHistory } from "react-router-dom";
 
 import {
@@ -16,6 +17,14 @@ import {
 } from "react-router-dom";
 
 const ButtonAppBar = () => {
+    const [openDialog, setDialog] = useState(false)
+
+    const openRecipe = () => {
+        setDialog(true)
+    }
+
+    const toggleModal = (val) => setDialog(val)
+
     const isProfilePage = AuthService.getCurrentUser();
     const history = useHistory();
 
@@ -25,46 +34,93 @@ const ButtonAppBar = () => {
         window.location.reload();
     }
     console.log(isProfilePage)
-    return (
-        <div className="appbar">
-            <AppBar position="static">
-                <Toolbar>
+    if (openDialog === true) {
+        return (
+            <div>
+                <RecipeDialog openDialog={openDialog} toggleModal={toggleModal}/>
+                <div className="appbar">
+                <AppBar position="static">
+                    <Toolbar>
 
-                    {isProfilePage === null ?
-                        <div className="appbar-container">
-                            <div className="appbar-logo" style={{marginLeft:"45%"}}>
-                                <Link to="/">
-                                    <img src={logo} alt="logo of the brand"></img>
-                                </Link>
-                            </div>
-                            <Link to="/login" style={{paddingTop:"20px"}}>
-                                <LogInButton />
-                            </Link>
-                            <Link to="/sign-up" style={{paddingTop:"20px"}}>
-                                <SignUpButton />
-                            </Link>
-                        </div>
-                        :
-                        <div className="appbar-container">
-                            <div>
-                                <div>
-                                    <Button className="appbar-button2" style={{padding:"30px"}} color="inherit" onClick={handleLogOut}>Add a New Recipe</Button>
+                        {isProfilePage === null ?
+                            <div className="appbar-container">
+                                <div className="appbar-logo" style={{ marginLeft: "45%" }}>
+                                    <Link to="/">
+                                        <img src={logo} alt="logo of the brand"></img>
+                                    </Link>
                                 </div>
-                            </div>
-                            <div className="appbar-logo">
-                                <Link to="/">
-                                    <img src={logo} alt="logo of the brand"></img>
+                                <Link to="/login" style={{ paddingTop: "20px" }}>
+                                    <LogInButton />
+                                </Link>
+                                <Link to="/sign-up" style={{ paddingTop: "20px" }}>
+                                    <SignUpButton />
                                 </Link>
                             </div>
-                            <Link to="/">
-                                <Button className="appbar-button1" style={{padding:"30px"}} color="inherit" onClick={handleLogOut}>Logout</Button>
-                            </Link>
-                        </div>
-                    }
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+                            :
+                            <div className="appbar-container">
+                                <div>
+                                    <div>
+                                        <Button className="appbar-button2" style={{ padding: "30px" }} color="inherit" onClick={openRecipe}>Add a New Recipe</Button>
+                                    </div>
+                                </div>
+                                <div className="appbar-logo">
+                                    <Link to="/">
+                                        <img src={logo} alt="logo of the brand"></img>
+                                    </Link>
+                                </div>
+                                <Link to="/">
+                                    <Button className="appbar-button1" style={{ padding: "30px" }} color="inherit" onClick={handleLogOut}>Logout</Button>
+                                </Link>
+                            </div>
+                        }
+                    </Toolbar>
+                </AppBar>
+            </div>
+            </div>
+            
+            )
+    } else {
+        return (
+            <div className="appbar">
+                <AppBar position="static">
+                    <Toolbar>
+
+                        {isProfilePage === null ?
+                            <div className="appbar-container">
+                                <div className="appbar-logo" style={{ marginLeft: "45%" }}>
+                                    <Link to="/">
+                                        <img src={logo} alt="logo of the brand"></img>
+                                    </Link>
+                                </div>
+                                <Link to="/login" style={{ paddingTop: "20px" }}>
+                                    <LogInButton />
+                                </Link>
+                                <Link to="/sign-up" style={{ paddingTop: "20px" }}>
+                                    <SignUpButton />
+                                </Link>
+                            </div>
+                            :
+                            <div className="appbar-container">
+                                <div>
+                                    <div>
+                                        <Button className="appbar-button2" style={{ padding: "30px" }} color="inherit" onClick={openRecipe}>Add a New Recipe</Button>
+                                    </div>
+                                </div>
+                                <div className="appbar-logo">
+                                    <Link to="/">
+                                        <img src={logo} alt="logo of the brand"></img>
+                                    </Link>
+                                </div>
+                                <Link to="/">
+                                    <Button className="appbar-button1" style={{ padding: "30px" }} color="inherit" onClick={handleLogOut}>Logout</Button>
+                                </Link>
+                            </div>
+                        }
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
 }
 
 export default ButtonAppBar;
