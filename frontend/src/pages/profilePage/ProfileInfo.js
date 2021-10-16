51bbd8f5ffc9ef6462f84cddc0d38a9304b71173
Hannes Wuorinen <hannes.wuorinen@tuni.fi>
2021-10-16 20:45:51 +0300

    profile page add recipegrid & recipeservice function

--- frontend/src/pages/profilePage/ProfileInfo.js
+++ frontend/src/pages/profilePage/ProfileInfo.js
@@ -1,21 +1,46 @@
 import { Typography } from "@material-ui/core";
 import Identicon from "react-identicons";
-import RecipeGrid from "../../components/RecipeGrid";
+import IconButton from "@material-ui/core/IconButton";
+import RecipeDialog from "../../components/RecipeDialog";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardHeader from "@material-ui/core/CardHeader";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import recipeimg from "../../img/pesto.jpg";
 import { Divider } from "@material-ui/core";
 import { Button } from "@material-ui/core";
+import recipeservice from "../../services/recipeservice";
+import { useEffect, useState } from "react";
 
 const ProfileInfo = ({ userData }) => {
+  const [userRecipes, setUserRecipes] = useState([]);
+  const [openDialog, setDialog] = useState(false);
+
+  const openRecipe = () => {
+    setDialog(true);
+  };
+
+  const toggleModal = (val) => setDialog(val);
+
+  useEffect(() => {
+    if (userData !== null) {
+      const getRecipes = recipeservice.getRecipesByUser(userData.id);
+      if (getRecipes !== undefined) {
+        setUserRecipes(getRecipes);
+      }
+    }
+  }, [userData]);
+
   if (userData != null)
     return (
       <div className="profile">
         <Divider />
         <div className="profile-info">
-          <Identicon
-            string={userData.username}
-            bg="#000000"
-            size="90"
-            style={{ borderRadius: "10px" }}
-          />
+          <Identicon string={userData.username} bg="#000000" size="90" />
           <div className="info">
             <Typography variant="h6" component="h2">
               {userData.username}
@@ -31,11 +56,51 @@ const ProfileInfo = ({ userData }) => {
             </Typography>
           </div>
           <Button size="small" variant="outlined" color="primary">
-            Edit
+            Edit profile
           </Button>
         </div>
         <Divider />
-        <RecipeGrid />
+        {userRecipes == undefined ||
+          (userRecipes == "" ? (
+            <div>No recipes found!</div>
+          ) : (
+            <div className="user-recipe-grid">
+              <Grid container spacing={5}>
+                {userRecipes.map((item) => (
+                  <Grid item xs={(2, 3)}>
+                    <Card className="recipe-card">
+                      <CardActionArea onClick={openRecipe}>
+                        <CardMedia
+                          component="img"
+                          alt="Picture of the recipe"
+                          height="120"
+                          image={recipeimg}
+                        />
+                        <CardHeader
+                          title={item.title}
+                          subheader={" by " + item.author}
+                        />
+                        <CardContent>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                          >
+                            {item.description}
+                          </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                          <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                          </IconButton>
+                        </CardActions>
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
+          ))}
       </div>
     );
   else {
--- frontend/src/services/recipeservice.js
+++ frontend/src/services/recipeservice.js
@@ -1,23 +1,39 @@
-import axios from 'axios'
-const baseUrl = '/'
+import axios from "axios";
+const baseUrl = "http://localhost:8080/api/recipes";
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response =>  response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 const deleteRecipe = async (id, newObject) => {
-  const response = await axios.delete(`${baseUrl}/${id}`, newObject)
-  return response.data
-}
+  const response = await axios.delete(`${baseUrl}/${id}`, newObject);
+  return response.data;
+};
 
+const getRecipesByUser = (userId) => {
+  const request = axios.get(`${baseUrl}/${userId}`);
+  request
+    .then((response) => {
+      console.log(response);
+      return response;
+    })
+    .catch((error) => {
+      if (error.response) {
+        return error.response;
+      } else if (error.request) {
+        return error.request;
+      } else {
+        return error.message;
+      }
+    });
+};
 
 const updateRecipe = async (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  const response = await request
-  return response.data
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  const response = await request;
+  return response.data;
+};
 
-const toBeExported =  { getAll, updateRecipe, deleteRecipe }
+const toBeExported = { getAll, updateRecipe, deleteRecipe, getRecipesByUser };
 
-export default toBeExported
-
+export default toBeExported;
